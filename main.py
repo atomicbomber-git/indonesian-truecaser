fd4805112161dd0e97c9f2d3810b8fee7a3038f8
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-05-26 19:26:01 +0700

    Working prototype.

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import Flask, jsonify, request, render_template
 import waitress
-import os
+import truecaser
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.jinja2")
+    output_text = None
+    input_text = None
+
+    if request.method == "POST":
+        # Remember old input
+        input_text = request.form["input_text"]
+        output_text = truecaser.process(request.form["input_text"])
+
+    return render_template("index.jinja2", input_text=input_text, output_text=output_text)
+
+@app.route("/handle", methods=["POST"])
+def handle():
+    text = request.form.get_json()
+    return jsonify(text)
 
 waitress.serve(app, host="0.0.0.0", port=8081)
--- truecaser.py
+++ truecaser.py
@@ -1,4 +1,32 @@
+import argparse
+from nltk import sent_tokenize
+
 def process(text):
+    
+    sentences = [list(sentence) for sentence in sent_tokenize(text)]
+    
+    for i, sentence in enumerate(sentences):
+        sentence[0] = sentence[0].upper()
+        sentences[i] = "".join(sentences[i])
+    
+    return " ".join(sentences)
 
-    pass
+def file_to_text(file):
+    return "".join([line for line in file])
 
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='''
+     Menerima input berupa file teks dan menghasilkan output berupa teks serupa, 
+     dengan penggunaan huruf kapital yang 
+     benar sesuai dengan aturan PUBI.
+    ''')
+    parser.add_argument('file', metavar='file', nargs=1, type=argparse.FileType('r'), help='file teks input')
+    
+    args = parser.parse_args()
+    file = args.file[0]
+
+    text = file_to_text(file)
+    result = process(text)
+
+    print(result)
